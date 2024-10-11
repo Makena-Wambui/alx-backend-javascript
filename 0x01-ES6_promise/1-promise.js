007ea603ca96363882ec8d75c5818ce5f71dff28
Makena-Wambui <iammakena@gmail.com>
2024-10-11 12:27:43 +0300

    Used a ternary operator to check if the promise was resolved or rejected.

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+// Using the prototype below, return a promise. The parameter is a boolean.
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-unused-expressions
+    success ? (resolve({
+      status: 200,
+      body: 'Success',
+    })) : (reject(new Error('The fake API is not working currently')));
+  });
+}
