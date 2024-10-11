3e34e98970c82eb339a0f76b1006dfcbe54d8175
Makena-Wambui <iammakena@gmail.com>
2024-10-11 13:05:57 +0300

    Fix logic in the return statement

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,15 +2,15 @@
 // Append three handlers to the function:
 export default function handleResponseFromAPI(promise) {
   // If the promise resolves, return an object with the following attributes:
-  // status: 200, body: success
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  }));
+  // status: 200, body: success.
+  // If the promise rejects, return an empty Error object
+  // Always log to the console 'Got a response from the API'
 
-  // When the Promise rejects, return an empty Error object
-  promise.catch(() => new Error());
-
-  // For every resolution, log Got a response from the API to the console
-  promise.finally(() => console.log('Got a response from the API'));
+  return promise
+    .finally(() => console.log('Got a response from the API'))
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error());
 }
