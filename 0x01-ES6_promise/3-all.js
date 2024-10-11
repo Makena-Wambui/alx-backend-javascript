0687f16fee9a2cbf4f9a6f7f2166a09faf5c7fc4
Makena-Wambui <iammakena@gmail.com>
2024-10-11 15:02:04 +0300

    Fixed function logic

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let body;
   // the functions in utils.js return promises
-  uploadPhoto().then((valueObj) => {
-    body = valueObj.body;
-    return body;
-  }).then(() => {
-    createUser().then((nameObj) => {
-      const { firstName, lastName } = nameObj;
-      console.log(`${body} ${firstName} ${lastName}`);
-    });
-  }).catch(() => console.log('Signup system offline'));
+  return uploadPhoto() // returns a promise
+    .then((valueObj) => {
+      // valueObj is the resolved value of the promise returned by uploadPhoto
+      // the resolved value is an object with a key 'body'
+      const body = valueObj.body;
+      return createUser()
+        .then((nameObj) => {
+          const { firstName, lastName } = nameObj;
+          console.log(`${body} ${firstName} ${lastName}`);
+        });
+    })
+    .catch(() => console.log('Signup system offline'));
 }
