48bbb43b16876f654b9ef38d6de6072a53fd18be
Makena-Wambui <iammakena@gmail.com>
2024-10-18 08:43:16 +0300

    Removed try catch block and used if else block to handle the error.

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -2,13 +2,9 @@ export default function divideFunction(numerator, denominator) {
   // if denominator is equal to 0,
   // the function should throw a new error with the message cannot divide by 0.
   // Otherwise it should return the numerator divided by the denominator.
-  try {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    } else {
-      return numerator / denominator;
-    }
-  } catch (err) {
-    return err;
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
   }
 }
